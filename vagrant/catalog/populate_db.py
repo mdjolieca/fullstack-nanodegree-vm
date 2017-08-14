69c7753c1c7418fd97a5e2279a235abd803c4ea7
mdjolieca <mdjolieca@gmail.com>
2017-08-14 19:32:57 -0400

    : from sqlalchemy import create_engine from sqlalchemy.orm import sessionmaker from database_setup import Category, Base, Item, User
    engine = create_engine('sqlite:///catalogue.db')
    
    DBSession = sessionmaker(bind=engine)
    session = DBSession()
    
    User0 = User(name="test user ", email="Your_email@gmail.com",
                 picture=' ')
    session.add(User0)
    session.commit()
    User1 = User(name="The Force", email="yoda@starwars.com",
                 picture='/static/yoda.jpg')
    session.add(User1)
    session.commit()
    
    description = "A great description of this category will" +
    " go here but for now this is a Sample description."
    
    for x in range(1, 10):
        if(x % 2) == 0:
            category = Category(user_id=User0.id, name='Category Name ' + str(x),
                                description=description)
        else:
            category = Category(user_id=User1.id, name='Category Name' + str(x),
                                description=description)
        print('created category ' + category.name + ':\n')
        session.add(category)
        session.commit()
        items = []
        for i in range(1, 5):
            items.append(Item(name='Item Name '+str(i),
                         description="Sample Item description item database id" +
                                     "number for testing is " + str((x-1)*5 + i),
                                     price="$0.01",  category_id=category.id))
        print(items)
        session.add_all(items)
        session.commit()
    
    session.close()
    engine.dispose()
    
    print "added sample data to catalogue tables!"

--- vagrant/catalog/populate_db.py
+++ vagrant/catalog/populate_db.py
@@ -4,10 +4,10 @@ from sqlalchemy.orm import sessionmaker
 from database_setup import Category, Base, Item, User
 
 engine = create_engine('sqlite:///catalogue.db')
-# Bind the engine to the metadata of the Base class 
+# Bind the engine to the metadata of the Base class
 
 DBSession = sessionmaker(bind=engine)
-#create DB session for adding data to tables
+# create DB session for adding data to tables
 session = DBSession()
 
 
@@ -23,32 +23,37 @@ User1 = User(name="The Force", email="yoda@starwars.com",
 session.add(User1)
 session.commit()
 
-#generic Category description
-description = "A great description of this category will go here but for now this is a Sample description."
+# generic Category description
+description = "A great description of this category will" +
+" go here but for now this is a Sample description."
 
 # Create Sample Categories with 5 items in each,
 # alternating between user_ids (see above) for Category owner and no owner
-# Each item's database id number appears in the item's description, 
+# Each item's database id number appears in the item's description,
 # this is useful for testing direct access to routes etc.
 
 for x in range(1, 10):
-    if(x%2)==0:
-       category = Category(user_id=User0.id, name='Category Name '+str(x), description=description)
+    if(x % 2) == 0:
+        category = Category(user_id=User0.id, name='Category Name ' + str(x),
+                            description=description)
     else:
-       category = Category(user_id=User1.id, name='Category Name'+str(x), description=description)
-    print('created category '+category.name+':\n')
+        category = Category(user_id=User1.id, name='Category Name' + str(x),
+                            description=description)
+    print('created category ' + category.name + ':\n')
     session.add(category)
     session.commit()
     items = []
     for i in range(1, 5):
-       items.append(Item( name='Item Name '+str(i), description="Sample Item description item database id number for testing is " + str((x-1)*5 + i),
-       price="$0.01",  category_id=category.id))
-       print(items)
+        items.append(Item(name='Item Name '+str(i),
+                     description="Sample Item description item database id" +
+                                 "number for testing is " + str((x-1)*5 + i),
+                                 price="$0.01",  category_id=category.id))
+    print(items)
     session.add_all(items)
     session.commit()
 
 
-#close session and destroy engine 
+# close session and destroy engine
 session.close()
 engine.dispose()
 
